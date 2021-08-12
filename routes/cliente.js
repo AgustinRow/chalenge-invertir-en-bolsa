dda15f9534cbbe33ff74acecfc51d18d37b62418
AgustinRow <agus.diaz.11@gmail.com>
2021-08-12 16:38:27 -0300

    fix: se borro carpeta sin uso

--- routes/cliente.js
+++
@@ -1,7 +0,0 @@
-const { Router } = require("express");
-const router = Router();
-const User = require("../controller/client");
-
-router.post("/client", User.addClient);
-
-module.exports = router;
